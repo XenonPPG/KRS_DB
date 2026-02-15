9cf4951edb77f59f1cbb4cd0c839921ba6193fbf
ART <popugajarcik@gmail.com>
2026-02-15 17:36:23 +0300

    transferred docker-compose, fixed DB initializer

--- app/internal/initializers/db.initializer.go
+++ app/internal/initializers/db.initializer.go
@@ -15,7 +15,7 @@ var DB *gorm.DB
 
 func ConnectDB(config models.Config) error {
 	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
-		config.PostgresUser, config.PostgresPassword, config.PostgresPort, config.PostgresHost, config.PostgresDB)
+		config.PostgresUser, config.PostgresPassword, config.PostgresHost, config.PostgresPort, config.PostgresDB)
 
 	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
