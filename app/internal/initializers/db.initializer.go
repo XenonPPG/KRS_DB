c6f9f2dd66741e3a4fa31e4453aa86ec1e49bab9
ART <popugajarcik@gmail.com>
2026-02-15 21:31:47 +0300

    Postgres migrations fix

--- app/internal/initializers/db.initializer.go
+++ app/internal/initializers/db.initializer.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"fmt"
+	"time"
 
 	descModels "github.com/XenonPPG/KRS_CONTRACTS/models"
 
@@ -17,11 +18,20 @@ func ConnectDB(config models.Config) error {
 	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
 		config.PostgresUser, config.PostgresPassword, config.PostgresHost, config.PostgresPort, config.PostgresDB)
 
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	var err error
+	// try 5 times to connect DB
+	for i := 0; i < 5; i++ {
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		fmt.Printf("Attempt %d failed: %v\n", i+1, err)
+		time.Sleep(time.Second * 3)
+	}
 	if err != nil {
 		return err
 	}
 
-	err = DB.AutoMigrate(&descModels.Note{})
+	err = DB.AutoMigrate(&descModels.User{}, &descModels.Note{})
 	return err
 }
